e1d42f71392887500e475526c632d9929b0f813e
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2022-03-16 13:52:11 +0900

    update JGH
    dd

---
+++ Assets/JGH/Script/TestScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    int iTemp = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //iTemp = Units.instance.GetSetInt;
+        //Debug.Log(iTemp);
+
+
+        //Units.instance.GetSetInt = 1;
+        //Debug.Log(Units.instance.GetSetInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/JGH/Script/Units.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Units : MonoBehaviour
+{
+
+ 
+
+
+}
