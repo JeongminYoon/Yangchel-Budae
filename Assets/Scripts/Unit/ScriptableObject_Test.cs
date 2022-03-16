ba5c7f02cf8a6a8f0c8c3d64441b293464b5bc86
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2022-03-16 12:04:46 +0900

    update Unit part
    upate + C# call by ref, valvue + C# class, struct

---
+++ Assets/Scripts/Unit/ScriptableObject_Test.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "UnitStatus", menuName = "Scriptable Object/UnitStatus", order= int.MaxValue)]
+public class ScriptableObject_Test : ScriptableObject
+{
+    [SerializeField] //접근지정자가 private라도 인스펙터 창에 뜨게 하는거
+    private float   hp;
+    [SerializeField]
+    private float   dmg;
+    [SerializeField]
+    private float   atkSpd;
+    [SerializeField]
+    private float   moveSpd;
+    [SerializeField]
+    private float   atkRagne;
+    [SerializeField]
+    private float   sightRange;
+    [SerializeField]
+    private int     cost;
+    [SerializeField]
+    private float   spawnTime;
+    
+    
+
+    public float GetSetHp;
+ 
+
+     public void SetHp(int value)
+     {
+
+     }
+
+    //public float GetHP()
+    //{ 
+    //    return hp;
+    //}
+
+    //public void SetHP(float _temp)
+    //   {
+    //    hp = _temp;
+    //}
+    public float GetSetDmg
+    {
+        get { return dmg; }
+        set { dmg = value; }
+    }
+
+
+    
+
+}
--- Assets/Scripts/Unit/Units.cs
+++ Assets/Scripts/Unit/Units.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class Units : MonoBehaviour
 {
+    public static Units instance = null;
+
+
+    ScriptableObject_Test temp = null;
     // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        int c = 10;
+        int d = 20;
+
+    }
+    //c# 클래스는 콜바이레퍼로 무조건 됨  + 힙 할당
+    //구조체는 콜바이밸류로  + 스택 할당ㄴ
+
+
     void Start()
     {
+
         
+        
+
     }
+
+
 
     // Update is called once per frame
     void Update()
