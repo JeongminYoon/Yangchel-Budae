6bb9d59d2e675d52674856ff19d8bf97b0d89aac
Teruminus <101047918+Teruminus@users.noreply.github.com>
2022-03-23 12:11:45 +0900

    update incoding

--- Assets/JGH/Script/DelegateTest.cs
+++ Assets/JGH/Script/DelegateTest.cs
@@ -7,13 +7,15 @@ public class DelegateTest : MonoBehaviour
     //c# 델리게이트, 이벤트 테스트
 
     //C# 델리게이트 => C++의 함수 포인터와 비슷한듯?
+    //요걸로 이벤트 만들어서 오브젝트 죽었을 때 처리 ㄱㄱㄱㄱ
+
 
     delegate int MyDelegate(int a);
 
     static int TestFunc(int a)
     {
         return a;
-    }
+    }    
 
     static void RunDelegate(MyDelegate func)
     {
